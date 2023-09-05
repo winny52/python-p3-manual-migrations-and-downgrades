06abeea3c5e70722ed5f507f7bff3a6edad0364c
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-05 10:05:01 +0300

    Cmpleted lab

--- lib/models.py
+++ lib/models.py
@@ -19,14 +19,12 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(), index=True)
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, index=True)  # Rename 'name' to 'full_name'
     email = Column(String(55))
-    grade = Column(Integer())
-    birthday = Column(DateTime())
-    enrolled_date = Column(DateTime(), default=datetime.now())
+    grade = Column(Integer)
+    birthday = Column(DateTime)
+    enrolled_date = Column(DateTime, default=datetime.now)
 
     def __repr__(self):
-        return f"Student {self.id}: " \
-            + f"{self.name}, " \
-            + f"Grade {self.grade}"
+        return f"Student {self.id}: {self.full_name}, Grade {self.grade}"
